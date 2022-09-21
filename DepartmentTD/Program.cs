bd08b43d6643c9713fd4fbf8c9d0c833a8f5c021
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-21 16:34:47 +0530

    Add project files.

---
+++ DepartmentTD/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepartmentTD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Department[] depart =
+            {
+                new Department{Id=1,Name="Art",Location="Akole"},
+                new Department{Id=2,Name="Commerce",Location="Jalgav"},
+                new Department{Id=3,Name="Science",Location="Parbhani"},
+                new Department{Id=4,Name="IT",Location="Satara"},
+                new Department{Id=5,Name="ITI",Location="Loni"}
+
+            };
+            foreach (Department d in depart)
+            {
+                Console.WriteLine(d);
+            }
+        }
+
+    }
+}
